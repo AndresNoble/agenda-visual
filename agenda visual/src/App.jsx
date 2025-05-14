d8ee873175cb3b8e9bc06fa94d6d025fbe46cf84
AndresNoble <andresnob910@gmail.com>
2025-05-13 21:25:51 -0300

    commit

---
+++ agenda visual/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "Arial" }}>
+      <h1>Agenda Visual</h1>
+      <p>Esta es una app de organización personalizada.</p>
+    </div>
+  );
+}
+
+export default App;
